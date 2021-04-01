9a158128c27e59c122abb4730c651e6450d174b6
grrttmrtn1 <49645329+grrttmrtn1@users.noreply.github.com>
2021-03-31 20:18:59 -0500

    Update reverse.ps1

--- reversetext/reverse.ps1
+++ reversetext/reverse.ps1
@@ -1,18 +1,10 @@
 #pass in file if stored in another directory. If run in the context of the folder, default to text file
-#add percentvariance as whole number i.e. 90 = 90%
 param (
-    $file = 'occurences.txt',
-    $percentVariance = 90
+    $file = 'reverseme.txt'
 )
-
+#get content of text file
 $text = Get-Content $file
-
-#assuming that the result was okay to see rare and common I would display like this 
-#$text.ToCharArray() | Group-Object -NoElement | Sort-Object -Property Count
-
-#count characters and group them
-$charArray = $text.ToCharArray() | Group-Object -NoElement
-#calculate average based upon sum of all counts
-$average = ($charArray | Measure-Object -Property Count -Sum).Sum/$charArray.Count
-#output all characters with a count with a higher variance than passed. By default higher than a 90% variance.
-$charArray | where Count -le ($average * (1 - $percentVariance/100))
+#iterate over the length of the text file contents in reverse. Write host defaults a new line.
+for ($i=$text.Length-1; $i -ge 0; $i--) {
+    Write-Host $text[$i] -NoNewline
+}
