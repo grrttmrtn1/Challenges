cbd0c0139a589cc9632c4a96ed97c963dc0ae385
grrttmrtn1 <49645329+grrttmrtn1@users.noreply.github.com>
2021-03-31 20:21:32 -0500

    Update test_assessment.py

--- JSONPlaceHolder/test_assessment.py
+++ JSONPlaceHolder/test_assessment.py
@@ -1,1 +1,20 @@
+import pytest
+from JSONPlaceHolder import JSONPlaceHolder
+import json
 
+Obj = JSONPlaceHolder('https://jsonplaceholder.typicode.com/posts') 
+
+def test_Get_ByID():
+    Request = Obj.Get_ByID(1)
+    assert Request.status_code == 200
+
+def test_Create_Post():
+    Request = Obj.Create_Post('Test Title', 500, 'This is a test')
+    assert Request.status_code == 201
+
+def test_Get_Title():
+    assert Obj.Get_Title(Obj.Get_ByID(1)) == 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit'
+
+def test_Delete_ByID():
+    Request = Obj.Delete_ByID(1)
+    assert Request.status_code == 200
