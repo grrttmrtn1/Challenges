5da14feb70d50671cbd1419a341c4c29458a75bf
grrttmrtn1 <49645329+grrttmrtn1@users.noreply.github.com>
2021-03-31 20:16:40 -0500

    Update sortips.ps1

--- sortips/sortips.ps1
+++ sortips/sortips.ps1
@@ -1,1 +1,12 @@
-
+#pass in files if stored in another directory. If run in the context of the folder, default to text file
+#inFile = input
+#outFile = output
+param (
+    $inFile = 'iplist.txt',
+    $outFile = 'results.txt'
+)
+$text = Get-Content $inFile
+#sort by octect and then convert back to string
+[string[]]$sortedIPs = [system.version[]]($text) | Sort-Object
+#output to file
+$sortedIPs | Out-File 'results.txt'
