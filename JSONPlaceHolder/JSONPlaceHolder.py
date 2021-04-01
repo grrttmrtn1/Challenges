96e979ac837028f1789565aedc5f06def116b55f
grrttmrtn1 <49645329+grrttmrtn1@users.noreply.github.com>
2021-03-31 20:21:06 -0500

    Update JSONPlaceHolder.py

--- JSONPlaceHolder/JSONPlaceHolder.py
+++ JSONPlaceHolder/JSONPlaceHolder.py
@@ -1,1 +1,21 @@
+import requests
+import json
 
+class JSONPlaceHolder:
+    def __init__(self, BaseURI):
+        self.BaseURI = BaseURI
+    def Get_ByID(self, id):
+        Uri = self.BaseURI + '/' + str(id)
+        Request = requests.get(Uri)
+        return Request
+    def Create_Post(self, Title, UserID, Body):
+        Headers =  {'Content-type': 'application/json; charset=UTF-8'}
+        RequestBody = {'title': Title, 'body': Body, 'userId': UserID}
+        Request = requests.post(self.BaseURI, headers=Headers, data=json.dumps(RequestBody))
+        return Request
+    def Get_Title(self, Request):
+        return json.loads(Request.content)['title']
+    def Delete_ByID(self, id):
+        Uri = self.BaseURI + '/' + str(id)
+        Request = requests.delete(Uri)
+        return Request
